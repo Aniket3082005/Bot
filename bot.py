dcc785b055906fd17a7c6331e071ce41ecf062ad
Aniket3082005 <aniket3082@gmail.com>
2026-01-09 11:03:42 +0530

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,25 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, ContextTypes, filters
+
+BOT_TOKEN = "7957102663:AAHPE7waIXIJAsy7vP10M3bkkpsn64aPDQk"
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        "👋 Hello!\n"
+        "Main test bot hoon.\n"
+        "Koi bhi message bhejo 🙂"
+    )
+
+async def reply(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    text = update.message.text
+    await update.message.reply_text(f"Tumne likha: {text}")
+
+def main():
+    app = ApplicationBuilder().token(BOT_TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, reply))
+    print("Bot chal raha hai...")
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
